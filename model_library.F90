986e0326c60214aa55bffa8016744e39eaf88e52
Beatriz Arellano <barellano@protonmail.com>
2026-04-03 20:43:27 +0100

    Includes carbonate chemistry module

--- model_library.F90
+++ model_library.F90
@@ -2,9 +2,10 @@ module ecosed_model_library
 
    use fabm_types, only: type_base_model_factory, type_base_model
 
+   use carbonate_chemistry
    use gas_exchange
    use nitrogen
-   use organic_matter
+   use om_degradation
    use oxygen
    use pelagic_ecosystem
 
@@ -28,11 +29,12 @@ contains
       class (type_base_model), pointer :: model
 
       select case (name)
-         case ('gas_exchange');    allocate (type_gas_exchange::model)
-         case ('nitrogen');        allocate (type_nitrogen::model)
-         case ('organic_matter');  allocate (type_organic_matter::model)
-         case ('oxygen');          allocate (type_oxygen::model)
-         case ('pelagiceco');      allocate (type_pelagic_ecosystem::model)
+         case ('carbonate_chemistry'); allocate (type_carbonate_chemistry::model)
+         case ('gas_exchange');        allocate (type_gas_exchange::model)
+         case ('nitrogen');            allocate (type_nitrogen::model)
+         case ('om_degradation');      allocate (type_om_degradation::model)
+         case ('oxygen');              allocate (type_oxygen::model)
+         case ('pelagiceco');          allocate (type_pelagic_ecosystem::model)
          
          ! Add case statements for new models here
       end select
