6d37266d7d0bd7dbd330ad59250cf24a478400d7
Beatriz Arellano <barellano@protonmail.com>
2026-06-21 21:39:46 +0100

    Adds reaction with manganese

--- src/iron.F90
+++ src/iron.F90
@@ -35,15 +35,23 @@ module iron
       ! --- Optional couplings
       type(type_state_variable_id) :: id_o2
       type(type_state_variable_id) :: id_alk
+      type(type_state_variable_id) :: id_mn2
+      type(type_state_variable_id) :: id_mno2
 
       ! --- Dependencies
       type(type_dependency_id) :: id_porosity
 
       ! --- Diagnostics
-      type(type_diagnostic_variable_id) :: id_fe_ox
+      type(type_diagnostic_variable_id) :: id_fe_o2_ox
+      type(type_diagnostic_variable_id) :: id_fe_mno2_ox
+      type(type_diagnostic_variable_id) :: id_alk_fe_o2_ox
+      type(type_diagnostic_variable_id) :: id_alk_fe_mno2_ox
 
       ! --- Parameters
       real(rk) :: k_fe_ox        ! Fe(II) oxidation rate constant (m3 mmol-1 s-1 internally)
+      real(rk) :: k_fe_mno2      ! Fe(II) oxidation by MnO2 rate constant (m3 mmol-1 s-1 internally)
+      logical  :: save_process_rates
+      logical  :: save_alkalinity_changes
 
    contains
       procedure :: initialize
@@ -64,7 +72,12 @@ contains
       ! ---------------- Parameters ----------------
       call self%get_parameter(self%k_fe_ox, 'k_fe_ox', 'm3 mmol-1 d-1', 'Second-order mass-action rate constant for Fe(II) oxidation by oxygen', &
                               default=0.02738_rk, scale_factor=d_per_s, minimum=0.0_rk)
+      call self%get_parameter(self%k_fe_mno2, 'k_fe_mno2', 'm3 mmol-1 d-1', 'Second-order rate constant for Fe(II) oxidation by MnO2', & 
+                              default=2.74e-5_rk, scale_factor=d_per_s, minimum=0.0_rk)
       call self%get_parameter(w_fe3ox, 'w_fe3ox', 'm d-1', 'Sinking velocity of particulate Fe(III) oxides', default=-1.0_rk, scale_factor=m_d_per_m_s)
+
+      call self%get_parameter(self%save_process_rates, 'process_rates', '', 'Save process-rate diagnostics', default=.false.)
+      call self%get_parameter(self%save_alkalinity_changes, 'alkalinity_changes', '', 'Save alkalinity-change diagnostics', default=.false.)
 
       ! ---------------- State variables ----------------
       call self%register_state_variable(self%id_fe2, 'fe2', 'mmol m-3', 'Dissolved Fe(II)', initial_value=0.001_rk, minimum=0.0_rk, no_river_dilution=.true.)
@@ -83,9 +96,19 @@ contains
       ! ---------------- Couplings ----------------
       call self%register_state_dependency(self%id_o2, 'o2', 'mmol m-3', 'Dissolved oxygen', required=.true.)
       call self%register_state_dependency(self%id_alk, 'alk', 'mmol eq m-3', 'Total alkalinity', required=.false.)
+      call self%register_state_dependency(self%id_mn2,  'mn2',  'mmol m-3', 'Dissolved Mn(II)', required=.false.)
+      call self%register_state_dependency(self%id_mno2, 'mno2', 'mmol m-3', 'Particulate MnO2', required=.false.)
 
       ! ---------------- Diagnostics ----------------
-      call self%register_diagnostic_variable(self%id_fe_ox, 'fe2_ox', 'mmol m-3 d-1', 'Fe(II) oxidation rate')
+      if (self%save_process_rates) then
+         call self%register_diagnostic_variable(self%id_fe_o2_ox, 'FE_OX', 'mmol m-3 d-1', 'Fe(II) oxidation rate by oxygen')
+         call self%register_diagnostic_variable(self%id_fe_mno2_ox, 'FE_MNO2_OX', 'mmol m-3 d-1', 'Fe(II) oxidation by MnO2')
+      end if
+
+      if (self%save_alkalinity_changes) then
+         call self%register_diagnostic_variable(self%id_alk_fe_o2_ox, 'ALK_FE_OX', 'mmol eq m-3 d-1', 'Alkalinity change due to Fe(II) oxidation by oxygen')
+         call self%register_diagnostic_variable(self%id_alk_fe_mno2_ox, 'ALK_FE_MNO2_OX', 'mmol eq m-3 d-1', 'Alkalinity change due to Fe(II) oxidation by MnO2')
+      end if
 
    end subroutine initialize
 
@@ -95,11 +118,13 @@ contains
       _DECLARE_ARGUMENTS_DO_
 
       real(rk) :: fe2, o2
-      real(rk) :: fe_ox
+      real(rk) :: mn2, mno2
+      real(rk) :: fe_ox, fe_mno2
       real(rk) :: o2_cons_fe_ox
       real(rk) :: alk_change
-      real(rk) :: phi, phi_s, d2p
-      real(rk) :: fe3ox_prod
+      real(rk) :: alk_fe_o2_change, alk_fe_mno2_change
+      real(rk) :: phi, phi_s, d2p, p2d
+      real(rk) :: fe3ox_prod, fe2_cons
 
       real(rk), parameter :: secs_per_day   = 86400.0_rk
       real(rk), parameter :: o2_per_fe_ox   = 0.25_rk
@@ -110,6 +135,10 @@ contains
 
          _GET_(self%id_fe2, fe2)
          _GET_(self%id_o2, o2)
+
+         alk_fe_mno2_change = 0.0_rk
+         alk_fe_o2_change   = 0.0_rk
+         alk_change         = 0.0_rk
 
          !--------------------------------------------------------------------
          ! Conversion factors for cross-phase reactions.
@@ -125,8 +154,10 @@ contains
          phi_s = max(1.0_rk - phi, 0.0_rk)
 
          if (phi > eps_phi .and. phi < 1.0_rk - eps_phi) then
+            p2d = phi_s / phi
             d2p = phi / phi_s
          else
+            p2d = 1.0_rk  
             d2p = 1.0_rk
          end if
 
@@ -151,20 +182,61 @@ contains
          ! phase to solid-phase concentration using d2p.
          fe_ox = self%k_fe_ox * max(fe2, 0.0_rk) * max(o2, 0.0_rk)
 
-         o2_cons_fe_ox = o2_per_fe_ox * fe_ox
-         alk_change    = alk_per_fe_ox * fe_ox
+         o2_cons_fe_ox    = o2_per_fe_ox * fe_ox
+         alk_fe_o2_change = alk_per_fe_ox * fe_ox
 
          ! Fe2+ is in the dissolved phase and Fe(OH)3/Fe2O3  in the solid phase
          ! Therefore multiplying by the conversion factor
          fe3ox_prod = d2p * fe_ox
+         fe2_cons   = -fe_ox
+         
+         
+         ! ------------------------------------------------------------------
+         ! Fe(II) oxidation by MnO2
+         !
+         ! 2Fe2+ + MnO2 + 2HCO3 + 2H2O -> 2Fe(OH)3 + Mn2+ + 2C02
+         !
+         ! Consumes dissolved Fe2 and MnO2, produces particulate Fe3ox and Mn2+.
+         ! Alkalinity decreases by 2 equivalents per mol reaction.
+         ! No explicit DIC change: HCO3- is converted to CO2.
+         ! Fe(II) oxidation is represented using second-order mass-action
+         ! kinetics:
+         !
+         !   rate = k_fe_mno2 * [Fe2+] * [MnO2]
+         ! ------------------------------------------------------------------      
+         fe_mno2 = 0.0_rk
+         if (_AVAILABLE_(self%id_mn2) .and. _AVAILABLE_(self%id_mno2)) then
+            _GET_(self%id_mn2,  mn2)
+            _GET_(self%id_mno2, mno2)
+            ! Reaction rate interpreted on the particulate/solid-volume basis.
+            fe_mno2 = self%k_fe_mno2 * max(fe2, 0.0_rk) * max(mno2, 0.0_rk)
+            alk_fe_mno2_change = -2.0_rk * p2d * fe_mno2
+            fe3ox_prod = fe3ox_prod + 2.0_rk * fe_mno2
+            fe2_cons   = fe2_cons   -2.0_rk * p2d  * fe_mno2
+         end if
 
-         _ADD_SOURCE_(self%id_fe2,   -fe_ox)
+         alk_change = alk_fe_o2_change + alk_fe_mno2_change
+
+         _ADD_SOURCE_(self%id_fe2,    fe2_cons)
          _ADD_SOURCE_(self%id_fe3ox,  fe3ox_prod)
 
          _ADD_SOURCE_(self%id_o2, -o2_cons_fe_ox)
          if (_AVAILABLE_(self%id_alk)) _ADD_SOURCE_(self%id_alk, alk_change)
 
-         _SET_DIAGNOSTIC_(self%id_fe_ox, fe_ox * secs_per_day)
+         if (fe_mno2 > 0.0_rk) then
+            _ADD_SOURCE_(self%id_mno2,  -fe_mno2)
+            _ADD_SOURCE_(self%id_mn2,    p2d *fe_mno2)
+         end if
+
+         if (self%save_process_rates) then
+            _SET_DIAGNOSTIC_(self%id_fe_o2_ox,   fe_ox   * secs_per_day)
+            _SET_DIAGNOSTIC_(self%id_fe_mno2_ox, fe_mno2 * secs_per_day)
+         end if
+
+         if (self%save_alkalinity_changes) then
+            _SET_DIAGNOSTIC_(self%id_alk_fe_o2_ox,   alk_fe_o2_change   * secs_per_day)
+            _SET_DIAGNOSTIC_(self%id_alk_fe_mno2_ox, alk_fe_mno2_change * secs_per_day)
+         end if
 
       _LOOP_END_
 
